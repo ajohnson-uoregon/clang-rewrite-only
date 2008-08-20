ajohnson-uoregon/clang-rewrite-only

commit 1eb329b1cf847d23807cdb29f1e4240ef7bccb85

Move the type checking that Sema::ActOnCastExpr does into a new Sema::CheckCastTypes function so that it can be reused.
llvm-svn: 54852

clang/lib/Sema/Sema.h
@@ -869,6 +869,9 @@ private:
   
   StringLiteral *IsStringLiteralInit(Expr *Init, QualType DeclType);
   bool CheckStringLiteralInit(StringLiteral *strLiteral, QualType &DeclT);
+
+  /// CheckCastTypes - Check type constraints for casting between types.
+  bool CheckCastTypes(SourceRange TyRange, QualType CastTy, Expr *CastExpr);
   
   // CheckVectorCast - check type constraints for vectors. 
   // Since vectors are an extension, there are no C standard reference for this.

clang/lib/Sema/SemaExpr.cpp
@@ -1088,6 +1088,42 @@ ActOnInitList(SourceLocation LBraceLoc, ExprTy **initlist, unsigned NumInit,
   return E;
 }
 
+/// CheckCastTypes - Check type constraints for casting between types.
+bool Sema::CheckCastTypes(SourceRange TyR, QualType castType, Expr *castExpr) {
+  UsualUnaryConversions(castExpr);
+
+  // C99 6.5.4p2: the cast type needs to be void or scalar and the expression
+  // type needs to be scalar.
+  if (castType->isVoidType()) {
+    // Cast to void allows any expr type.
+  } else if (!castType->isScalarType() && !castType->isVectorType()) {
+    // GCC struct/union extension: allow cast to self.
+    if (Context.getCanonicalType(castType) !=
+        Context.getCanonicalType(castExpr->getType()) ||
+        (!castType->isStructureType() && !castType->isUnionType())) {
+      // Reject any other conversions to non-scalar types.
+      return Diag(TyR.getBegin(), diag::err_typecheck_cond_expect_scalar, 
+                  castType.getAsString(), castExpr->getSourceRange());
+    }
+      
+    // accept this, but emit an ext-warn.
+    Diag(TyR.getBegin(), diag::ext_typecheck_cast_nonscalar, 
+         castType.getAsString(), castExpr->getSourceRange());
+  } else if (!castExpr->getType()->isScalarType() && 
+             !castExpr->getType()->isVectorType()) {
+    return Diag(castExpr->getLocStart(), 
+                diag::err_typecheck_expect_scalar_operand, 
+                castExpr->getType().getAsString(),castExpr->getSourceRange());
+  } else if (castExpr->getType()->isVectorType()) {
+    if (CheckVectorCast(TyR, castExpr->getType(), castType))
+      return true;
+  } else if (castType->isVectorType()) {
+    if (CheckVectorCast(TyR, castType, castExpr->getType()))
+      return true;
+  }
+  return false;
+}
+
 bool Sema::CheckVectorCast(SourceRange R, QualType VectorTy, QualType Ty) {
   assert(VectorTy->isVectorType() && "Not a vector type!");
   
@@ -1116,39 +1152,8 @@ ActOnCastExpr(SourceLocation LParenLoc, TypeTy *Ty,
   Expr *castExpr = static_cast<Expr*>(Op);
   QualType castType = QualType::getFromOpaquePtr(Ty);
 
-  UsualUnaryConversions(castExpr);
-
-  // C99 6.5.4p2: the cast type needs to be void or scalar and the expression
-  // type needs to be scalar.
-  if (castType->isVoidType()) {
-    // Cast to void allows any expr type.
-  } else if (!castType->isScalarType() && !castType->isVectorType()) {
-    // GCC struct/union extension: allow cast to self.
-    if (Context.getCanonicalType(castType) !=
-        Context.getCanonicalType(castExpr->getType()) ||
-        (!castType->isStructureType() && !castType->isUnionType())) {
-      // Reject any other conversions to non-scalar types.
-      return Diag(LParenLoc, diag::err_typecheck_cond_expect_scalar, 
-                  castType.getAsString(), castExpr->getSourceRange());
-    }
-      
-    // accept this, but emit an ext-warn.
-    Diag(LParenLoc, diag::ext_typecheck_cast_nonscalar, 
-         castType.getAsString(), castExpr->getSourceRange());
-  } else if (!castExpr->getType()->isScalarType() && 
-             !castExpr->getType()->isVectorType()) {
-    return Diag(castExpr->getLocStart(), 
-                diag::err_typecheck_expect_scalar_operand, 
-                castExpr->getType().getAsString(),castExpr->getSourceRange());
-  } else if (castExpr->getType()->isVectorType()) {
-    if (CheckVectorCast(SourceRange(LParenLoc, RParenLoc), 
-                        castExpr->getType(), castType))
-      return true;
-  } else if (castType->isVectorType()) {
-    if (CheckVectorCast(SourceRange(LParenLoc, RParenLoc), 
-                        castType, castExpr->getType()))
-      return true;
-  }
+  if (CheckCastTypes(SourceRange(LParenLoc, RParenLoc), castType, castExpr))
+    return true;
   return new CastExpr(castType, castExpr, LParenLoc);
 }
 

commit 07fbf88b2ff5fced84ce50860d2d1c289b13efe5

Fix subtle bug introduced in r54852.  - UsualUnaryConversions takes an Expr *& and may modify its argument,    this broke when it was refactored into Sema::CheckCastTypes. This    meant that we were missing implicit casts in some places.  - Seems pretty sad that this got through our tests.
llvm-svn: 55039

clang/lib/Sema/Sema.h
@@ -871,7 +871,7 @@ private:
   bool CheckStringLiteralInit(StringLiteral *strLiteral, QualType &DeclT);
 
   /// CheckCastTypes - Check type constraints for casting between types.
-  bool CheckCastTypes(SourceRange TyRange, QualType CastTy, Expr *CastExpr);
+  bool CheckCastTypes(SourceRange TyRange, QualType CastTy, Expr *&CastExpr);
   
   // CheckVectorCast - check type constraints for vectors. 
   // Since vectors are an extension, there are no C standard reference for this.

clang/lib/Sema/SemaExpr.cpp
@@ -1089,7 +1089,7 @@ ActOnInitList(SourceLocation LBraceLoc, ExprTy **initlist, unsigned NumInit,
 }
 
 /// CheckCastTypes - Check type constraints for casting between types.
-bool Sema::CheckCastTypes(SourceRange TyR, QualType castType, Expr *castExpr) {
+bool Sema::CheckCastTypes(SourceRange TyR, QualType castType, Expr *&castExpr) {
   UsualUnaryConversions(castExpr);
 
   // C99 6.5.4p2: the cast type needs to be void or scalar and the expression

clang/test/CodeGen/2008-08-19-cast-of-typedef.c
@@ -1,0 +1,10 @@
+// RUN: clang --emit-llvm -o %t %s
+
+typedef short T[4];
+struct s {
+  T f0;
+};
+
+void foo(struct s *x) {
+  bar((long) x->f0);
+}