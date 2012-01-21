ajohnson-uoregon/clang-rewrite-only

commit 818dbfa0434cc278e8274c5557478f56913bb18e

[analyzer] Tweak the buildbot script to include the experimental taint checker and correct the checker enabling setting.
llvm-svn: 148558

clang/utils/analyzer/SATestBuild.py
@@ -72,7 +72,7 @@ SBOutputDirReferencePrefix = "Ref"
 
 # The list of checkers used during analyzes.
 # Currently, consists of all the non experimental checkers.
-Checkers="core,deadcode,cplusplus,security,unix,osx,cocoa"
+Checkers="experimental.security.taint,core,deadcode,cplusplus,security,unix,osx,cocoa"
 
 Verbose = 1
 
@@ -171,7 +171,7 @@ def runAnalyzePreprocessed(Dir, SBOutputDir):
         raise Exception()       
 
     CmdPrefix = "clang -cc1 -analyze -analyzer-output=plist -w "
-    CmdPrefix += "-analyzer-checker=" + Checkers +" -fcxx-exceptions -fblocks "   
+    CmdPrefix += "-enable-checker " + Checkers +" -fcxx-exceptions -fblocks "   
     
     PlistPath = os.path.join(Dir, SBOutputDir, "date")
     FailPath = os.path.join(PlistPath, "failures");

commit 14315a32277a34af911ee4d0a0c5583cb73a5e84

[analyzer] Unbreak the static analyzer bot. Regression from r148558.
When we build a single source file, we call clang directly (not though scan-build).

llvm-svn: 148612

clang/utils/analyzer/SATestBuild.py
@@ -171,7 +171,7 @@ def runAnalyzePreprocessed(Dir, SBOutputDir):
         raise Exception()       
 
     CmdPrefix = "clang -cc1 -analyze -analyzer-output=plist -w "
-    CmdPrefix += "-enable-checker " + Checkers +" -fcxx-exceptions -fblocks "   
+    CmdPrefix += "-analyzer-checker=" + Checkers +" -fcxx-exceptions -fblocks "   
     
     PlistPath = os.path.join(Dir, SBOutputDir, "date")
     FailPath = os.path.join(PlistPath, "failures");