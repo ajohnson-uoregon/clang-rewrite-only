ajohnson-uoregon/clang-rewrite-only

commit c8cbd442741faa5f17d616c6e5e129208ae21000

Fix PR26585 by improving the promotion of DBG_VALUEs to DW_AT_locations. When a variable is described by a single DBG_VALUE instruction we can often use a more efficient inline DW_AT_location instead of using a location list.
This commit makes the heuristic that decides when to apply this
optimization stricter by also verifying that the DBG_VALUE is live at the
entry of the function (instead of just checking that it is valid until
the end of the function).

<rdar://problem/24611008>

llvm-svn: 262247

llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
@@ -935,6 +935,18 @@ DbgVariable *DwarfDebug::createConcreteVariable(LexicalScope &Scope,
   return ConcreteVariables.back().get();
 }
 
+// Determine whether this DBG_VALUE is valid at the beginning of the function.
+static bool validAtEntry(const MachineInstr *MInsn) {
+  auto MBB = MInsn->getParent();
+  // Is it in the entry basic block?
+  if (!MBB->pred_empty())
+    return false;
+  for (MachineBasicBlock::const_reverse_iterator I(MInsn); I != MBB->rend(); ++I)
+    if (!(I->isDebugValue() || I->getFlag(MachineInstr::FrameSetup)))
+      return false;
+  return true;
+}
+
 // Find variables for each lexical scope.
 void DwarfDebug::collectVariableInfo(DwarfCompileUnit &TheCU,
                                      const DISubprogram *SP,
@@ -967,8 +979,11 @@ void DwarfDebug::collectVariableInfo(DwarfCompileUnit &TheCU,
     const MachineInstr *MInsn = Ranges.front().first;
     assert(MInsn->isDebugValue() && "History must begin with debug value");
 
-    // Check if the first DBG_VALUE is valid for the rest of the function.
-    if (Ranges.size() == 1 && Ranges.front().second == nullptr) {
+    // Check if there is a single DBG_VALUE, valid throughout the function.
+    // A single constant is also considered valid for the entire function.
+    if (Ranges.size() == 1 &&
+        (MInsn->getOperand(0).isImm() ||
+         (validAtEntry(MInsn) && Ranges.front().second == nullptr))) {
       RegVar->initializeDbgValue(MInsn);
       continue;
     }

llvm/test/DebugInfo/X86/PR26148.ll
@@ -19,7 +19,7 @@
 ; AS in 26163, we expect two ranges (as opposed to one), the first one being zero sized
 ;
 ;
-; CHECK: 0x00000000: Beginning address offset: 0x0000000000000004
+; CHECK: 0x00000025: Beginning address offset: 0x0000000000000004
 ; CHECK:                Ending address offset: 0x0000000000000004
 ; CHECK:                 Location description: 10 03 55 93 04
 ; CHECK:             Beginning address offset: 0x0000000000000004

llvm/test/DebugInfo/X86/parameters.ll
@@ -25,15 +25,19 @@
 ; CHECK: debug_info contents
 ; 0x74 is DW_OP_breg4, showing that the parameter is accessed indirectly
 ; (with a zero offset) from the register parameter
-; CHECK: DW_AT_location{{.*}}(<0x0{{.}}> 74 00
+; CHECK: DW_AT_location [DW_FORM_data4]	([[F_LOC:0x[0-9]*]])
 ; CHECK-NOT: DW_TAG
 ; CHECK: DW_AT_name{{.*}} = "f"
-
+;
 ; CHECK: DW_AT_location{{.*}}([[G_LOC:0x[0-9]*]])
 ; CHECK-NOT: DW_TAG
 ; CHECK: DW_AT_name{{.*}} = "g"
+;
 ; CHECK: debug_loc contents
-; CHECK-NEXT: [[G_LOC]]: Beginning
+; CHECK:         [[F_LOC]]: Beginning
+; CHECK-NEXT:               Ending
+; CHECK-NEXT: Location description: 74 00
+; CHECK:         [[G_LOC]]: Beginning
 ; CHECK-NEXT:               Ending
 ; CHECK-NEXT: Location description: 74 00
 

llvm/test/DebugInfo/X86/pieces-2.ll
@@ -17,9 +17,14 @@
 ;
 ;
 ; CHECK: DW_TAG_variable [4]
-;                                                  rax, piece 0x00000004
-; CHECK-NEXT: DW_AT_location [DW_FORM_block1]{{.*}}50 93 04
+; CHECK-NEXT:   DW_AT_location [DW_FORM_data4]        ([[LOC:.*]])
 ; CHECK-NEXT:  DW_AT_name {{.*}}"i1"
+;
+; CHECK: .debug_loc
+; CHECK: [[LOC]]: Beginning address offset: 0x0000000000000004
+; CHECK-NEXT:        Ending address offset: 0x0000000000000005
+;                                           rax, piece 0x00000004
+; CHECK-NEXT:         Location description: 50 93 04
 ;
 ; ModuleID = '/Volumes/Data/llvm/test/DebugInfo/X86/sroasplit-1.ll'
 target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"

llvm/test/DebugInfo/X86/pieces-3.ll
@@ -16,19 +16,22 @@
 ;    }
 ;
 ; CHECK: DW_TAG_formal_parameter [3]
-; CHECK-NEXT:   DW_AT_location [DW_FORM_data4]        ([[LOC:.*]])
+; CHECK-NEXT:   DW_AT_location [DW_FORM_data4]        ([[LOC1:.*]])
 ; CHECK-NEXT:   DW_AT_name {{.*}}"outer"
 ; CHECK: DW_TAG_variable
-;                                                 rsi, piece 0x00000004
-; CHECK-NEXT:   DW_AT_location [DW_FORM_block1]       {{.*}} 54 93 04
+; CHECK-NEXT:   DW_AT_location [DW_FORM_data4]        ([[LOC2:.*]])
 ; CHECK-NEXT:   "i1"
 ;
 ; CHECK: .debug_loc
-; CHECK: [[LOC]]:
-; CHECK: Beginning address offset: 0x0000000000000000
-; CHECK:    Ending address offset: 0x0000000000000008
-; rdi, piece 0x00000008, piece 0x00000004, rsi, piece 0x00000004
-; CHECK: Location description: 55 93 08 93 04 54 93 04 
+; CHECK: [[LOC1]]: Beginning address offset: 0x0000000000000000
+; CHECK:              Ending address offset: 0x0000000000000008
+;             rdi, piece 0x00000008, piece 0x00000004, rsi, piece 0x00000004
+; CHECK-NEXT: Location description: 55 93 08 93 04 54 93 04
+; CHECK: [[LOC2]]: Beginning address offset: 0x0000000000000004
+; CHECK-NEXT:         Ending address offset: 0x0000000000000008
+;                                     rsi, piece 0x00000004
+; CHECK-NEXT:   Location description: 54 93 04
+
 ;
 ; ModuleID = '/Volumes/Data/llvm/test/DebugInfo/X86/sroasplit-2.ll'
 target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"

llvm/test/DebugInfo/X86/reference-argument.ll
@@ -1,8 +1,9 @@
-; RUN: llc -mtriple=x86_64-apple-macosx10.9.0 -filetype=obj -O0 < %s | llvm-dwarfdump -debug-dump=info - | FileCheck %s
+; RUN: llc -mtriple=x86_64-apple-macosx10.9.0 -filetype=obj -O0 < %s | llvm-dwarfdump -debug-dump=all - | FileCheck %s
 ; ModuleID = 'aggregate-indirect-arg.cpp'
 ; extracted from debuginfo-tests/aggregate-indirect-arg.cpp
 
 ; v should be a pointer.
+; CHECK:  .debug_info contents:
 ; CHECK:   DW_TAG_subprogram
 ; CHECK:     DW_AT_specification {{.*}} "_ZN1A3fooE4SVal"
 ; CHECK-NOT: DW_TAG_subprogram
@@ -10,9 +11,11 @@
 ; CHECK:       DW_AT_name {{.*}} "this"
 ; CHECK-NOT:   DW_TAG_subprogram
 ; CHECK:     DW_TAG_formal_parameter
-;                                                    rsi+0
-; CHECK-NEXT:  DW_AT_location [DW_FORM_block1]      (<0x02> 74 00{{ *}})
+; CHECK-NEXT:  DW_AT_location [DW_FORM_data4]	(0x00000000)
 ; CHECK-NEXT:  DW_AT_name {{.*}} "v"
+; CHECK: .debug_loc contents:
+;                                rsi+0
+; CHECK:   Location description: 74 00
 
 target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128"
 target triple = "x86_64-apple-macosx10.9.0"

llvm/test/DebugInfo/X86/single-dbg_value.ll
@@ -1,0 +1,71 @@
+; RUN: %llc_dwarf -stop-after=livedebugvalues -o /dev/null %s 2>&1 \
+; RUN:   | FileCheck %s --check-prefix=SANITY
+; RUN: %llc_dwarf -march=x86-64 -o - %s -filetype=obj \
+; RUN:   | llvm-dwarfdump -debug-dump=all - | FileCheck %s
+;
+; CHECK: .debug_info contents:
+; CHECK: DW_TAG_variable
+; CHECK-NEXT:   DW_AT_location [DW_FORM_data4]
+; CHECK-NEXT:   DW_AT_name{{.*}}"a"
+; CHECK: .debug_loc contents:
+;                               rax, piece 0x00000004
+; CHECK:  Location description: 50 93 04
+; SANITY: DBG_VALUE
+; SANITY-NOT: DBG_VALUE
+; ModuleID = 'test.ll'
+; Compiled with -O:
+;   void h(int);
+;   int g();
+;   void f() {
+;     h(0);
+;     int a = g();
+;     h(a);
+;   }
+target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx"
+
+; Function Attrs: nounwind ssp uwtable
+define void @f() #0 !dbg !4 {
+entry:
+  tail call void @h(i32 0) #2, !dbg !14
+  %call = tail call i32 (...) @g() #2, !dbg !15
+  tail call void @llvm.dbg.value(metadata i32 %call, i64 0, metadata !8, metadata !16), !dbg !17
+  tail call void @h(i32 %call) #2, !dbg !18
+  ret void, !dbg !19
+}
+
+declare void @h(i32)
+
+declare i32 @g(...)
+
+; Function Attrs: nounwind readnone
+declare void @llvm.dbg.value(metadata, i64, metadata, metadata) #1
+
+attributes #0 = { nounwind ssp uwtable "no-frame-pointer-elim"="true" }
+attributes #1 = { nounwind readnone }
+attributes #2 = { nounwind }
+
+!llvm.dbg.cu = !{!0}
+!llvm.module.flags = !{!10, !11, !12}
+!llvm.ident = !{!13}
+
+!0 = distinct !DICompileUnit(language: DW_LANG_C99, file: !1, producer: "clang version 3.9.0 ", isOptimized: true, runtimeVersion: 0, emissionKind: 1, enums: !2, subprograms: !3)
+!1 = !DIFile(filename: "test.c", directory: "/Volumes/Data/llvm")
+!2 = !{}
+!3 = !{!4}
+!4 = distinct !DISubprogram(name: "f", scope: !1, file: !1, line: 3, type: !5, isLocal: false, isDefinition: true, scopeLine: 3, isOptimized: true, variables: !7)
+!5 = !DISubroutineType(types: !6)
+!6 = !{null}
+!7 = !{!8}
+!8 = !DILocalVariable(name: "a", scope: !4, file: !1, line: 5, type: !9)
+!9 = !DIBasicType(name: "int", size: 32, align: 32, encoding: DW_ATE_signed)
+!10 = !{i32 2, !"Dwarf Version", i32 2}
+!11 = !{i32 2, !"Debug Info Version", i32 3}
+!12 = !{i32 1, !"PIC Level", i32 2}
+!13 = !{!"clang version 3.9.0 "}
+!14 = !DILocation(line: 4, column: 3, scope: !4)
+!15 = !DILocation(line: 5, column: 11, scope: !4)
+!16 = !DIExpression()
+!17 = !DILocation(line: 5, column: 7, scope: !4)
+!18 = !DILocation(line: 6, column: 3, scope: !4)
+!19 = !DILocation(line: 7, column: 1, scope: !4)

llvm/test/DebugInfo/X86/subregisters.ll
@@ -6,8 +6,13 @@
 ;
 ; rdar://problem/16015314
 ;
-; CHECK:  DW_AT_location [DW_FORM_block1]       (<0x03> 54 93 04 )
-; CHECK:  DW_AT_name [DW_FORM_strp]{{.*}} "a"
+; CHECK:  .debug_info contents:
+; CHECK:  DW_TAG_variable
+; CHECK-NEXT:  DW_AT_location [DW_FORM_data4]	(0x00000000)
+; CHECK-NEXT:  DW_AT_name [DW_FORM_strp]{{.*}} "a"
+; CHECK: .debug_loc contents:
+;                                    rsi, piece 0x00000004
+; CHECK:       Location description: 54 93 04 
 ;
 ; struct bar {
 ;   int a;

commit 03f2b94169e82ff87742dbc4ace1bb08119b17b0

DwarfDebug: Support floating point constants in location lists.
This patch closes a gap in the DWARF backend that caused LLVM to drop
debug info for floating point variables that were constant for part of
their scope. Floating point constants are emitted as one or more
DW_OP_constu joined via DW_OP_piece.

This fixes a regression caught by the LLDB testsuite that I introduced
in r262247 when we stopped blindly expanding the range of singular
DBG_VALUEs to span the entire scope and started to emit location lists
with accurate ranges instead.

Also deletes a now-impossible testcase (debug-loc-empty-entries).

<rdar://problem/25448338>

llvm-svn: 265760

llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
@@ -1438,10 +1438,10 @@ static void emitDebugLocValue(const AsmPrinter &AP, const DIBasicType *BT,
         DwarfExpr.AddMachineRegExpression(Expr, Loc.getReg(),
                                           PieceOffsetInBits);
     }
+  } else if (Value.isConstantFP()) {
+    APInt RawBytes = Value.getConstantFP()->getValueAPF().bitcastToAPInt();
+    DwarfExpr.AddUnsignedConstant(RawBytes);
   }
-  // else ... ignore constant fp. There is not any good way to
-  // to represent them here in dwarf.
-  // FIXME: ^
 }
 
 void DebugLocEntry::finalize(const AsmPrinter &AP,

llvm/lib/CodeGen/AsmPrinter/DwarfExpression.cpp
@@ -159,29 +159,37 @@ bool DwarfExpression::AddMachineRegPiece(unsigned MachineReg,
   return CurPos > PieceOffsetInBits;
 }
 
+void DwarfExpression::AddStackValue() {
+  if (DwarfVersion >= 4)
+    EmitOp(dwarf::DW_OP_stack_value);
+}
+
 void DwarfExpression::AddSignedConstant(int Value) {
   EmitOp(dwarf::DW_OP_consts);
   EmitSigned(Value);
-  // The proper way to describe a constant value is
-  // DW_OP_constu <const>, DW_OP_stack_value.
-  // Unfortunately, DW_OP_stack_value was not available until DWARF-4,
-  // so we will continue to generate DW_OP_constu <const> for DWARF-2
-  // and DWARF-3. Technically, this is incorrect since DW_OP_const <const>
-  // actually describes a value at a constant addess, not a constant value.
-  // However, in the past there was no better way  to describe a constant
-  // value, so the producers and consumers started to rely on heuristics
-  // to disambiguate the value vs. location status of the expression.
-  // See PR21176 for more details.
-  if (DwarfVersion >= 4)
-    EmitOp(dwarf::DW_OP_stack_value);
+  AddStackValue();
 }
 
 void DwarfExpression::AddUnsignedConstant(unsigned Value) {
   EmitOp(dwarf::DW_OP_constu);
   EmitUnsigned(Value);
-  // cf. comment in DwarfExpression::AddSignedConstant().
-  if (DwarfVersion >= 4)
-    EmitOp(dwarf::DW_OP_stack_value);
+  AddStackValue();
+}
+
+void DwarfExpression::AddUnsignedConstant(APInt Value) {
+  unsigned Size = Value.getBitWidth();
+  const uint64_t *Data = Value.getRawData();
+
+  // Chop it up into 64-bit pieces, because that's the maximum that
+  // AddUnsignedConstant takes.
+  unsigned Offset = 0;
+  while (Offset < Size) {
+    AddUnsignedConstant(*Data++);
+    if (Offset == 0 && Size <= 64)
+      break;
+    AddOpPiece(std::min(Size-Offset, 64u), Offset);
+    Offset += 64;
+  }
 }
 
 static unsigned getOffsetOrZero(unsigned OffsetInBits,

llvm/lib/CodeGen/AsmPrinter/DwarfExpression.h
@@ -61,6 +61,19 @@ public:
   void AddOpPiece(unsigned SizeInBits, unsigned OffsetInBits = 0);
   /// Emit a shift-right dwarf expression.
   void AddShr(unsigned ShiftBy);
+  /// Emit a DW_OP_stack_value, if supported.
+  ///
+  /// The proper way to describe a constant value is
+  /// DW_OP_constu <const>, DW_OP_stack_value.
+  /// Unfortunately, DW_OP_stack_value was not available until DWARF-4,
+  /// so we will continue to generate DW_OP_constu <const> for DWARF-2
+  /// and DWARF-3. Technically, this is incorrect since DW_OP_const <const>
+  /// actually describes a value at a constant addess, not a constant value.
+  /// However, in the past there was no better way  to describe a constant
+  /// value, so the producers and consumers started to rely on heuristics
+  /// to disambiguate the value vs. location status of the expression.
+  /// See PR21176 for more details.
+  void AddStackValue();
 
   /// Emit an indirect dwarf register operation for the given machine register.
   /// \return false if no DWARF register exists for MachineReg.
@@ -87,6 +100,8 @@ public:
   void AddSignedConstant(int Value);
   /// Emit an unsigned constant.
   void AddUnsignedConstant(unsigned Value);
+  /// Emit an unsigned constant.
+  void AddUnsignedConstant(APInt Value);
 
   /// \brief Emit an entire expression on top of a machine register location.
   ///

llvm/test/DebugInfo/X86/debug-loc-empty-entries.ll
@@ -1,66 +1,0 @@
-; RUN: llc -mtriple=x86_64-apple-macosx <%s | FileCheck %s
-; Test that we don't generate empty .debug_loc entries.  Originally, there were
-; two empty .debug_loc entries for 'a' in an otherwise empty .debug_loc list.
-;
-; CHECK:      .section __DWARF,__debug_loc,regular,debug
-; CHECK-NEXT: Lsection_debug_loc:
-; CHECK-NEXT: .section __DWARF,__debug_abbrev,regular,debug
-;
-; Test that the variable stuck around.
-; CHECK:      .section __DWARF,__debug_info,regular,debug
-; CHECK:      DW_TAG_variable
-; CHECK-NOT:  DW_AT_location
-
-; Generated using clang -cc1 with the following args:
-;
-;   -triple x86_64-apple-macosx -emit-llvm -gdwarf-4 -O1
-;
-; From this testcase:
-;
-;;   void fn1() {
-;;     float a = 1;
-;;     for (;;)
-;;       a = 0;
-;;   }
-
-; Function Attrs: noreturn nounwind readnone
-define void @_Z3fn1v() #0 !dbg !4 {
-entry:
-  tail call void @llvm.dbg.value(metadata float 1.000000e+00, i64 0, metadata !9, metadata !14), !dbg !15
-  br label %for.cond, !dbg !16
-
-for.cond:                                         ; preds = %for.cond, %entry
-  tail call void @llvm.dbg.value(metadata float 0.000000e+00, i64 0, metadata !9, metadata !14), !dbg !15
-  br label %for.cond, !dbg !17
-}
-
-; Function Attrs: nounwind readnone
-declare void @llvm.dbg.value(metadata, i64, metadata, metadata) #1
-
-attributes #0 = { noreturn nounwind readnone "less-precise-fpmad"="false" "no-frame-pointer-elim"="false" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "no-realign-stack" "stack-protector-buffer-size"="8" "target-features"="+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
-attributes #1 = { nounwind readnone }
-
-!llvm.dbg.cu = !{!0}
-!llvm.module.flags = !{!11, !12}
-!llvm.ident = !{!13}
-
-!0 = distinct !DICompileUnit(language: DW_LANG_C_plus_plus, file: !1, producer: "clang version 3.7.0 (trunk 238517) (llvm/trunk 238524)", isOptimized: true, runtimeVersion: 0, emissionKind: FullDebug, enums: !2, retainedTypes: !2, subprograms: !3, globals: !2, imports: !2)
-!1 = !DIFile(filename: "<stdin>", directory: "/Users/dexonsmith/data/llvm/bootstrap/play/delta2/testcase")
-!2 = !{}
-!3 = !{!4}
-!4 = distinct !DISubprogram(name: "fn1", linkageName: "_Z3fn1v", scope: !5, file: !5, line: 1, type: !6, isLocal: false, isDefinition: true, scopeLine: 1, flags: DIFlagPrototyped, isOptimized: true, variables: !8)
-!5 = !DIFile(filename: "t.cpp", directory: "/Users/dexonsmith/data/llvm/bootstrap/play/delta2/testcase")
-!6 = !DISubroutineType(types: !7)
-!7 = !{null}
-!8 = !{!9}
-!9 = !DILocalVariable(name: "a", scope: !4, file: !5, line: 2, type: !10)
-!10 = !DIBasicType(name: "float", size: 32, align: 32, encoding: DW_ATE_float)
-!11 = !{i32 2, !"Dwarf Version", i32 4}
-!12 = !{i32 2, !"Debug Info Version", i32 3}
-!13 = !{!"clang version 3.7.0 (trunk 238517) (llvm/trunk 238524)"}
-!14 = !DIExpression()
-!15 = !DILocation(line: 2, scope: !4)
-!16 = !DILocation(line: 3, scope: !4)
-!17 = !DILocation(line: 3, scope: !18)
-!18 = distinct !DILexicalBlock(scope: !19, file: !5, line: 3)
-!19 = distinct !DILexicalBlock(scope: !4, file: !5, line: 3)

llvm/test/DebugInfo/X86/float_const_loclist.ll
@@ -1,0 +1,86 @@
+; RUN: llc %s -stop-after=livedebugvalues -o %t 2>&1 | FileCheck --check-prefix=SANITY %s
+; RUN: llc < %s -filetype=obj | llvm-dwarfdump - | FileCheck %s
+; Test debug_loc support for floating point constants.
+;
+; Created from clang -O1:
+;   void barrier();
+;   void foo() {
+;     float f;
+;     long double ld;
+;     barrier();
+;     f = 3.14;
+;     ld = 3.14;
+;     barrier();
+;   }
+;
+; SANITY: CALL{{.*}} @barrier
+; SANITY: DBG_VALUE x86_fp80 0xK4000C8F5C28F5C28F800
+; SANITY: DBG_VALUE float 0x40091EB860000000
+; SANITY: TAILJMP{{.*}} @barrier
+;
+; CHECK: .debug_info contents:
+; CHECK: DW_TAG_variable
+; CHECK-NEXT:  DW_AT_location {{.*}} (0x00000000)
+; CHECK-NEXT:  DW_AT_name {{.*}}"ld"
+; CHECK: DW_TAG_variable
+; CHECK-NEXT:  DW_AT_location {{.*}} (0x00000031)
+; CHECK-NEXT:  DW_AT_name {{.*}}"f"
+;
+; CHECK: .debug_loc contents:
+; CHECK: 0x00000000: Beginning address offset: [[START:.*]]
+; CHECK:                Ending address offset: [[END:.*]]
+; CHECK:                 Location description: 10 80 f0 a3 e1 05 93 08 10 80 80 01 9d 10 40
+;                        constu ..., piece 8, constu ..., bit-piece 16 64
+; CHECK: 0x00000031: Beginning address offset: [[START]]
+; CHECK:                Ending address offset: [[END]]
+; CHECK:                 Location description: 10 c3 eb a3 82 04
+;                                              constu ...
+source_filename = "test.c"
+target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.11.0"
+
+; Function Attrs: nounwind ssp uwtable
+define void @foo() #0 !dbg !4 {
+entry:
+  tail call void (...) @barrier() #3, !dbg !16
+  tail call void @llvm.dbg.value(metadata float 0x40091EB860000000, i64 0, metadata !8, metadata !17), !dbg !18
+  tail call void @llvm.dbg.value(metadata x86_fp80 0xK4000C8F5C28F5C28F800, i64 0, metadata !10, metadata !17), !dbg !19
+  tail call void (...) @barrier() #3, !dbg !20
+  ret void, !dbg !21
+}
+
+declare void @barrier(...)
+
+; Function Attrs: nounwind readnone
+declare void @llvm.dbg.value(metadata, i64, metadata, metadata) #2
+
+attributes #0 = { nounwind ssp uwtable }
+attributes #2 = { nounwind readnone }
+attributes #3 = { nounwind }
+
+!llvm.dbg.cu = !{!0}
+!llvm.module.flags = !{!12, !13, !14}
+!llvm.ident = !{!15}
+
+!0 = distinct !DICompileUnit(language: DW_LANG_C99, file: !1, producer: "clang version 3.9.0 (trunk 265328) (llvm/trunk 265330)", isOptimized: true, runtimeVersion: 0, emissionKind: FullDebug, enums: !2, subprograms: !3)
+!1 = !DIFile(filename: "test.c", directory: "/Volumes/Data/radar/25448338")
+!2 = !{}
+!3 = !{!4}
+!4 = distinct !DISubprogram(name: "foo", scope: !1, file: !1, line: 3, type: !5, isLocal: false, isDefinition: true, scopeLine: 4, isOptimized: true, variables: !7)
+!5 = !DISubroutineType(types: !6)
+!6 = !{null}
+!7 = !{!8, !10}
+!8 = !DILocalVariable(name: "f", scope: !4, file: !1, line: 5, type: !9)
+!9 = !DIBasicType(name: "float", size: 32, align: 32, encoding: DW_ATE_float)
+!10 = !DILocalVariable(name: "ld", scope: !4, file: !1, line: 6, type: !11)
+!11 = !DIBasicType(name: "long double", size: 128, align: 128, encoding: DW_ATE_float)
+!12 = !{i32 2, !"Dwarf Version", i32 2}
+!13 = !{i32 2, !"Debug Info Version", i32 3}
+!14 = !{i32 1, !"PIC Level", i32 2}
+!15 = !{!"clang version 3.9.0 (trunk 265328) (llvm/trunk 265330)"}
+!16 = !DILocation(line: 7, column: 3, scope: !4)
+!17 = !DIExpression()
+!18 = !DILocation(line: 5, column: 9, scope: !4)
+!19 = !DILocation(line: 6, column: 15, scope: !4)
+!20 = !DILocation(line: 10, column: 3, scope: !4)
+!21 = !DILocation(line: 11, column: 1, scope: !4)