ajohnson-uoregon/clang-rewrite-only

commit 8de0beaea25b0eeb53ffdeb4fda1733d9ccce31f

[asan] Add missing declaration in the internal interface
Differential Revision: https://reviews.llvm.org/D29230

llvm-svn: 293423

compiler-rt/lib/asan/asan_interface_internal.h
@@ -242,6 +242,9 @@ extern "C" {
   void __asan_alloca_poison(uptr addr, uptr size);
   SANITIZER_INTERFACE_ATTRIBUTE
   void __asan_allocas_unpoison(uptr top, uptr bottom);
+
+  SANITIZER_INTERFACE_ATTRIBUTE SANITIZER_WEAK_ATTRIBUTE
+  const char* __asan_default_suppressions();
 }  // extern "C"
 
 #endif  // ASAN_INTERFACE_INTERNAL_H

commit 1154f9fe322ec3f9c9d64b13cfa49461f327030c

[asan] Fix tests for exported interfaces.
Add "OPTIONAL" comment to declaration of weak function in the internal
interface. This fix the tests `interface_symbols_linux.c` and
`interface_symbols_darwin.c` which were failing after r293423.

llvm-svn: 293442

compiler-rt/lib/asan/asan_interface_internal.h
@@ -244,7 +244,7 @@ extern "C" {
   void __asan_allocas_unpoison(uptr top, uptr bottom);
 
   SANITIZER_INTERFACE_ATTRIBUTE SANITIZER_WEAK_ATTRIBUTE
-  const char* __asan_default_suppressions();
+  /* OPTIONAL */ const char* __asan_default_suppressions();
 }  // extern "C"
 
 #endif  // ASAN_INTERFACE_INTERNAL_H

compiler-rt/test/asan/TestCases/Darwin/interface_symbols_darwin.c
@@ -10,6 +10,7 @@
 // RUN:   | grep "__asan_" | sed "s/___asan_/__asan_/" \
 // RUN:   | sed -E "s/__asan_version_mismatch_check_v[0-9]+/__asan_version_mismatch_check/" \
 // RUN:   | grep -v "__asan_default_options" \
+// RUN:   | grep -v "__asan_default_suppressions" \
 // RUN:   | grep -v "__asan_on_error" > %t.symbols
 
 // RUN: cat %p/../../../../lib/asan/asan_interface_internal.h \

compiler-rt/test/asan/TestCases/Linux/interface_symbols_linux.c
@@ -5,6 +5,7 @@
 // RUN:    | grep "__asan_" | sed "s/___asan_/__asan_/" \
 // RUN:    | sed -E "s/__asan_version_mismatch_check_v[0-9]+/__asan_version_mismatch_check/" \
 // RUN:    | grep -v "__asan_default_options" \
+// RUN:    | grep -v "__asan_default_suppressions" \
 // RUN:    | grep -v "__asan_stack_" \
 // RUN:    | grep -v "__asan_on_error" > %t.symbols
 // RUN: cat %p/../../../../lib/asan/asan_interface_internal.h \