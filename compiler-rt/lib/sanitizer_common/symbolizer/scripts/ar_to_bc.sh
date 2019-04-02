ajohnson-uoregon/clang-rewrite-only

commit 6f72b562fcae10e0d2369de3914f38e8d78af0bc

ar_to_bc.sh: replace readlink -f with custom relative path resolution
llvm-ar is a crunchgen-style executable dispatching to dlltool,ranlib,lib,ar based on argv[0].
In our content-addressable storage, readlink -f resolves paths to some
digest and thus lost the original "llvm-ar" filename.

Replace it with a custom path resolution to fix the problem.

llvm-svn: 357450

compiler-rt/lib/sanitizer_common/symbolizer/scripts/ar_to_bc.sh
@@ -9,8 +9,8 @@ if [ "$#" -le 1 ]; then
   usage
 fi
 
-AR=$(readlink -f $AR)
-LINK=$(readlink -f $LINK)
+[[ AR == /* ]] || AR=$PWD/$AR
+[[ LINK == /* ]] || LINK=$PWD/$LINK
 
 INPUTS=
 OUTPUT=

commit 0ae3036533f79dbfea412dd6a0d301dc286c5f61

Fix problem with ar_to_bc.sh script introduced by r357450.
The new code wasn't matching absolute paths correctly. Should unbreak
sanitizer-x86_64-linux bot.

llvm-svn: 357502

compiler-rt/lib/sanitizer_common/symbolizer/scripts/ar_to_bc.sh
@@ -9,8 +9,8 @@ if [ "$#" -le 1 ]; then
   usage
 fi
 
-[[ AR == /* ]] || AR=$PWD/$AR
-[[ LINK == /* ]] || LINK=$PWD/$LINK
+[[ $AR == /* ]] || AR=$PWD/$AR
+[[ $LINK == /* ]] || LINK=$PWD/$LINK
 
 INPUTS=
 OUTPUT=