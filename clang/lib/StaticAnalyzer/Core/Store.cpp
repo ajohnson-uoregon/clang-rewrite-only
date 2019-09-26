ajohnson-uoregon/clang-rewrite-only

commit 235db850dc899756846d9e28b514973af2a70c9b

[analyzer] Avoid small vectors of non-default-constructibles.
Unconfuses certain compilers.

llvm-svn: 372942

clang/lib/StaticAnalyzer/Core/Store.cpp
@@ -52,7 +52,7 @@ StoreRef StoreManager::enterStackFrame(Store OldStore,
   Call.getInitialStackFrameContents(LCtx, InitialBindings);
 
   for (const auto &I : InitialBindings)
-    Store = Bind(Store.getStore(), I.first, I.second);
+    Store = Bind(Store.getStore(), I.first.castAs<Loc>(), I.second);
 
   return Store;
 }

clang/include/clang/StaticAnalyzer/Core/PathSensitive/CallEvent.h
@@ -347,7 +347,7 @@ public:
   ProgramStateRef invalidateRegions(unsigned BlockCount,
                                     ProgramStateRef Orig = nullptr) const;
 
-  using FrameBindingTy = std::pair<Loc, SVal>;
+  using FrameBindingTy = std::pair<SVal, SVal>;
   using BindingsTy = SmallVectorImpl<FrameBindingTy>;
 
   /// Populates the given SmallVector with the bindings in the callee's stack

commit 1d68d137b516e0dfed469d022f2b8041ba1c2eb2

[analyzer] A speculative attempt to avoid gcc-7 crashes caused by r372942.
llvm-svn: 372945

clang/lib/StaticAnalyzer/Core/Store.cpp
@@ -51,8 +51,11 @@ StoreRef StoreManager::enterStackFrame(Store OldStore,
   SmallVector<CallEvent::FrameBindingTy, 16> InitialBindings;
   Call.getInitialStackFrameContents(LCtx, InitialBindings);
 
-  for (const auto &I : InitialBindings)
-    Store = Bind(Store.getStore(), I.first.castAs<Loc>(), I.second);
+  for (const auto &I : InitialBindings) {
+    Loc L = I.first.castAs<Loc>();
+    SVal V = I.second;
+    Store = Bind(Store.getStore(), L, V);
+  }
 
   return Store;
 }