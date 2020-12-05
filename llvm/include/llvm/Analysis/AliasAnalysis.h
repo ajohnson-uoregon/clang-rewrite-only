ajohnson-uoregon/clang-rewrite-only

commit e03349ef4059c25d623a56273d89158df3aef48e

[AA] Add statistics for alias results (NFC)
Count how many NoAlias/MustAlias/MayAlias we get from top-level
queries.

llvm/include/llvm/Analysis/AliasAnalysis.h
@@ -814,6 +814,9 @@ private:
 
   std::vector<AnalysisKey *> AADeps;
 
+  /// Query depth used to distinguish recursive queries.
+  unsigned Depth;
+
   friend class BatchAAResults;
 };
 

llvm/lib/Analysis/AliasAnalysis.cpp
@@ -24,6 +24,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/ADT/Statistic.h"
 #include "llvm/Analysis/BasicAliasAnalysis.h"
 #include "llvm/Analysis/CFLAndersAliasAnalysis.h"
 #include "llvm/Analysis/CFLSteensAliasAnalysis.h"
@@ -54,7 +55,13 @@
 #include <functional>
 #include <iterator>
 
+#define DEBUG_TYPE "aa"
+
 using namespace llvm;
+
+STATISTIC(NumNoAlias,   "Number of NoAlias results");
+STATISTIC(NumMayAlias,  "Number of MayAlias results");
+STATISTIC(NumMustAlias, "Number of MustAlias results");
 
 /// Allow disabling BasicAA from the AA results. This is particularly useful
 /// when testing to isolate a single AA implementation.
@@ -109,12 +116,25 @@ AliasResult AAResults::alias(const MemoryLocation &LocA,
 
 AliasResult AAResults::alias(const MemoryLocation &LocA,
                              const MemoryLocation &LocB, AAQueryInfo &AAQI) {
+  AliasResult Result = MayAlias;
+
+  Depth++;
   for (const auto &AA : AAs) {
-    auto Result = AA->alias(LocA, LocB, AAQI);
+    Result = AA->alias(LocA, LocB, AAQI);
     if (Result != MayAlias)
-      return Result;
+      break;
   }
-  return MayAlias;
+  Depth--;
+
+  if (Depth == 0) {
+    if (Result == NoAlias)
+      ++NumNoAlias;
+    else if (Result == MustAlias)
+      ++NumMustAlias;
+    else
+      ++NumMayAlias;
+  }
+  return Result;
 }
 
 bool AAResults::pointsToConstantMemory(const MemoryLocation &Loc,

commit 509d7e31b43e0e7ed97b3def7f9822dd458ded74

[AA] Initialize Depth member
Fix mistake introduced in e03349ef4059c25d623a56273d89158df3aef48e:
I failed to initialize the Depth member to zero.

llvm/include/llvm/Analysis/AliasAnalysis.h
@@ -815,7 +815,7 @@ private:
   std::vector<AnalysisKey *> AADeps;
 
   /// Query depth used to distinguish recursive queries.
-  unsigned Depth;
+  unsigned Depth = 0;
 
   friend class BatchAAResults;
 };