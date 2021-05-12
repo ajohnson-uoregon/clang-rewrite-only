ajohnson-uoregon/clang-rewrite-only

commit 09d74c14be940475de9177f0199eee980bf4fc5b

Revert "[Passes] Enable the relative lookup table converter pass on aarch64"
This reverts commit b1a3ed93d4134021b29566a4b5722a1f979bbced.

The relative lookup table converter pass seems to cause problems
for chromium on Windows/ARM64, see https://crbug.com/1204788.

llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -450,6 +450,11 @@ public:
     if (!TargetTriple.isArch64Bit())
       return false;
 
+    // TODO: Triggers an issue in aarch64, so temporarily disable it.
+    // See https://reviews.llvm.org/D99572 for more information.
+    if (TargetTriple.getArch() == Triple::aarch64)
+      return false;
+
     return true;
   }
 

commit 2ceef35368fe29e6bd6a954c8048175623c55353

[Passes] Reenable the relative lookup table converter pass for ELF and COFF on aarch64
The bug (PR50227, affecting COFF) that caused the revert in
09d74c14be940475de9177f0199eee980bf4fc5b has been fixed in
075979d3806a7b77a3237476d4dd1cf9f9ec7d6c now, so it should be safe
to reenable the pass for that target (and ELF).

In PR50227 it's also mentioned that the same pass seems to cause
problems on aarch64 on darwin, so leaving it disabled there for now.

llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -450,9 +450,9 @@ public:
     if (!TargetTriple.isArch64Bit())
       return false;
 
-    // TODO: Triggers an issue in aarch64, so temporarily disable it.
-    // See https://reviews.llvm.org/D99572 for more information.
-    if (TargetTriple.getArch() == Triple::aarch64)
+    // TODO: Triggers issues on aarch64 on darwin, so temporarily disable it
+    // there.
+    if (TargetTriple.getArch() == Triple::aarch64 && TargetTriple.isOSDarwin())
       return false;
 
     return true;