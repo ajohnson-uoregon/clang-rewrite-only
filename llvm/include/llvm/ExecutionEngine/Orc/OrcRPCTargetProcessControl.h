ajohnson-uoregon/clang-rewrite-only

commit 5c425a7d695491f6fbcdaf4d6c803076927b8778

[ORC] Switch from uint8_t to char buffers for TargetProcessControl::runWrapper.
This matches WrapperFunctionResult's char buffer, cutting down on the number of
pointer casts needed.

llvm/include/llvm/ExecutionEngine/Orc/OrcRPCTargetProcessControl.h
@@ -356,7 +356,7 @@ public:
 
   Expected<shared::WrapperFunctionResult>
   runWrapper(JITTargetAddress WrapperFnAddr,
-             ArrayRef<uint8_t> ArgBuffer) override {
+             ArrayRef<char> ArgBuffer) override {
     DEBUG_WITH_TYPE("orc", {
       dbgs() << "Running as wrapper function "
              << formatv("{0:x16}", WrapperFnAddr) << " with "

llvm/include/llvm/ExecutionEngine/Orc/Shared/Serialization.h
@@ -58,6 +58,11 @@ public:
   static const char *getName() { return "void"; }
 };
 
+template <> class SerializationTypeName<char> {
+public:
+  static const char *getName() { return "char"; }
+};
+
 template <> class SerializationTypeName<int8_t> {
 public:
   static const char *getName() { return "int8_t"; }

llvm/include/llvm/ExecutionEngine/Orc/TargetProcess/OrcRPCTPCServer.h
@@ -358,7 +358,7 @@ public:
 class RunWrapper
     : public shared::RPCFunction<RunWrapper,
                                  shared::WrapperFunctionResult(
-                                     JITTargetAddress, std::vector<uint8_t>)> {
+                                     JITTargetAddress, std::vector<char>)> {
 public:
   static const char *getName() { return "RunWrapper"; }
 };
@@ -580,14 +580,12 @@ private:
         ProgramNameOverride);
   }
 
-  shared::WrapperFunctionResult
-  runWrapper(JITTargetAddress WrapperFnAddr,
-             const std::vector<uint8_t> &ArgBuffer) {
+  shared::WrapperFunctionResult runWrapper(JITTargetAddress WrapperFnAddr,
+                                           const std::vector<char> &ArgBuffer) {
     using WrapperFnTy = shared::detail::CWrapperFunctionResult (*)(
         const char *Data, uint64_t Size);
     auto *WrapperFn = jitTargetAddressToFunction<WrapperFnTy>(WrapperFnAddr);
-    return WrapperFn(reinterpret_cast<const char *>(ArgBuffer.data()),
-                     ArgBuffer.size());
+    return WrapperFn(ArgBuffer.data(), ArgBuffer.size());
   }
 
   void closeConnection() { Finished = true; }

llvm/include/llvm/ExecutionEngine/Orc/TargetProcessControl.h
@@ -145,7 +145,7 @@ public:
   /// \endcode{.cpp}
   ///
   virtual Expected<shared::WrapperFunctionResult>
-  runWrapper(JITTargetAddress WrapperFnAddr, ArrayRef<uint8_t> ArgBuffer) = 0;
+  runWrapper(JITTargetAddress WrapperFnAddr, ArrayRef<char> ArgBuffer) = 0;
 
   /// Disconnect from the target process.
   ///
@@ -170,9 +170,7 @@ public:
       : TPC(TPC), WrapperFnAddr(WrapperFnAddr) {}
   Expected<shared::WrapperFunctionResult> operator()(const char *ArgData,
                                                      size_t ArgSize) const {
-    return TPC.runWrapper(
-        WrapperFnAddr,
-        ArrayRef<uint8_t>(reinterpret_cast<const uint8_t *>(ArgData), ArgSize));
+    return TPC.runWrapper(WrapperFnAddr, ArrayRef<char>(ArgData, ArgSize));
   }
 
 private:
@@ -204,8 +202,7 @@ public:
                               ArrayRef<std::string> Args) override;
 
   Expected<shared::WrapperFunctionResult>
-  runWrapper(JITTargetAddress WrapperFnAddr,
-             ArrayRef<uint8_t> ArgBuffer) override;
+  runWrapper(JITTargetAddress WrapperFnAddr, ArrayRef<char> ArgBuffer) override;
 
   Error disconnect() override;
 

llvm/lib/ExecutionEngine/Orc/TargetProcessControl.cpp
@@ -104,12 +104,11 @@ SelfTargetProcessControl::runAsMain(JITTargetAddress MainFnAddr,
 
 Expected<shared::WrapperFunctionResult>
 SelfTargetProcessControl::runWrapper(JITTargetAddress WrapperFnAddr,
-                                     ArrayRef<uint8_t> ArgBuffer) {
+                                     ArrayRef<char> ArgBuffer) {
   using WrapperFnTy = shared::detail::CWrapperFunctionResult (*)(
       const char *Data, uint64_t Size);
   auto *WrapperFn = jitTargetAddressToFunction<WrapperFnTy>(WrapperFnAddr);
-  return WrapperFn(reinterpret_cast<const char *>(ArgBuffer.data()),
-                   ArgBuffer.size());
+  return WrapperFn(ArgBuffer.data(), ArgBuffer.size());
 }
 
 Error SelfTargetProcessControl::disconnect() { return Error::success(); }

commit db3df627d07f03bb34370d99e6de35f29f496e48

[ORC] Use uint8_t rather than char for RPC wrapper-function calls.
This partially reverts 5c425a7d695, which broke some Solaris bots. Apparently
Solaris defines int8_t as char rather than signed char, which made the
SerializationTypeName<char> specialization a redefinition.

This partial revert isolates use of uint8_t buffers to ORC-RPC handling of
wrapper functions only. The TargetProcessControl::runWrapper method will
continue to use char buffers.

llvm/include/llvm/ExecutionEngine/Orc/OrcRPCTargetProcessControl.h
@@ -362,8 +362,10 @@ public:
              << formatv("{0:x16}", WrapperFnAddr) << " with "
              << formatv("{0:x16}", ArgBuffer.size()) << " argument buffer\n";
     });
-    auto Result =
-        EP.template callB<orcrpctpc::RunWrapper>(WrapperFnAddr, ArgBuffer);
+    auto Result = EP.template callB<orcrpctpc::RunWrapper>(
+        WrapperFnAddr,
+        ArrayRef<uint8_t>(reinterpret_cast<const uint8_t *>(ArgBuffer.data()),
+                          ArgBuffer.size()));
     return Result;
   }
 

llvm/include/llvm/ExecutionEngine/Orc/Shared/Serialization.h
@@ -58,11 +58,6 @@ public:
   static const char *getName() { return "void"; }
 };
 
-template <> class SerializationTypeName<char> {
-public:
-  static const char *getName() { return "char"; }
-};
-
 template <> class SerializationTypeName<int8_t> {
 public:
   static const char *getName() { return "int8_t"; }

llvm/include/llvm/ExecutionEngine/Orc/TargetProcess/OrcRPCTPCServer.h
@@ -358,7 +358,7 @@ public:
 class RunWrapper
     : public shared::RPCFunction<RunWrapper,
                                  shared::WrapperFunctionResult(
-                                     JITTargetAddress, std::vector<char>)> {
+                                     JITTargetAddress, std::vector<uint8_t>)> {
 public:
   static const char *getName() { return "RunWrapper"; }
 };
@@ -580,12 +580,14 @@ private:
         ProgramNameOverride);
   }
 
-  shared::WrapperFunctionResult runWrapper(JITTargetAddress WrapperFnAddr,
-                                           const std::vector<char> &ArgBuffer) {
+  shared::WrapperFunctionResult
+  runWrapper(JITTargetAddress WrapperFnAddr,
+             const std::vector<uint8_t> &ArgBuffer) {
     using WrapperFnTy = shared::detail::CWrapperFunctionResult (*)(
         const char *Data, uint64_t Size);
     auto *WrapperFn = jitTargetAddressToFunction<WrapperFnTy>(WrapperFnAddr);
-    return WrapperFn(ArgBuffer.data(), ArgBuffer.size());
+    return WrapperFn(reinterpret_cast<const char *>(ArgBuffer.data()),
+                     ArgBuffer.size());
   }
 
   void closeConnection() { Finished = true; }